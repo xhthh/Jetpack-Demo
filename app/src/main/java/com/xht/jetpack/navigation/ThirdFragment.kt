638492de61b22cc4c12030e1fcce3d587e678d29
xuhaitao <xuhaitao@9fbank.com.cn>
2020-12-25 16:30:25 +0800

    popUpTo退出

--- app/src/main/java/com/xht/jetpack/navigation/ThirdFragment.kt
+++ app/src/main/java/com/xht/jetpack/navigation/ThirdFragment.kt
@@ -5,23 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.xht.jetpack.R
+import kotlinx.android.synthetic.main.fragment_third.*
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class ThirdFragment : Fragment() {
-
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,14 +20,12 @@ class ThirdFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_third, container, false)
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ThirdFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //返回首页FirstFragment，<action/> 中使用 popUpTo
+        btnBackToHOme.setOnClickListener {
+            findNavController().navigate(R.id.thirdFragment_exit)
+        }
     }
 }
